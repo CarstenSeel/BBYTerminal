66f2ae9a41312ca6c9811c64ebff511c5a341f79
carsten.seel <carsten.seel@pcon.as>
2026-01-10 10:57:04 +0100

    a

--- src/app/apointments/apointments.module.ts
+++ src/app/apointments/apointments.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { DateAdapter, MatButtonModule, MatCardModule, MatDatepickerModule, MatDialogModule, MatDividerModule, MatFormFieldModule, MatIconModule, MatInputModule, MatMenuModule, MatNativeDateModule, MatOptionModule, MatRadioModule, MatSelectModule } from "@angular/material";
-import { ApointmentsComponent } from "./apointments.component";
 import { FlexLayoutModule } from "@angular/flex-layout";
 import { ReactiveFormsModule } from "@angular/forms";
 import { BrowserModule } from "@angular/platform-browser";
@@ -11,9 +10,7 @@ import { adapterFactory } from "angular-calendar/date-adapters/date-fns";
 import { OwlDateTimeModule, OwlNativeDateTimeModule } from "ng-pick-datetime";
 import { ChartsModule } from "ng2-charts";
 import { AppRoutingModule } from "../app-routing.module";
-import { CreateDialogComponent } from "./dialogs/createDialog/createDialog.component";
-import { DeleteDialogComponent } from "./dialogs/deleteDialog/deleteDialog.component";
-import { EditDialogComponent } from "./dialogs/editDialog/editDialog.component";
+import { ApointmentsComponent } from "./apointments.component";
 
 @NgModule({
     declarations: [ApointmentsComponent],
