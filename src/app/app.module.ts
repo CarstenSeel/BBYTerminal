bac2ce9a994421b7a779fb6dd5dd14809a3a5046
carsten.seel <carsten.seel@pcon.as>
2026-01-10 11:31:11 +0100

    test 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { FoodComponent } from './food/food.component';
-import { SleepComponent } from './sleep/sleep.component';
-import { SizeComponent } from './size/size.component';
-import { DiapersComponent } from './diapers/diapers.component';
 import { AppRoutingModule } from './/app-routing.module';
 import {MatCardModule} from '@angular/material/card';
 import {MatIconModule} from '@angular/material/icon';
@@ -14,10 +9,6 @@ import { MAT_DATE_LOCALE, MatButtonModule, MatDatepickerModule, MatDialogModule,
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ReactiveFormsModule } from '@angular/forms';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { ApointmentsComponent } from './apointments/apointments.component';
-import { WeightComponent } from './weight/weight.component';
-import { HeaderComponent } from './header/header.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
 import { DeleteDialogComponent } from './apointments/dialogs/deleteDialog/deleteDialog.component';
 import { DataShareService } from './Service/dataShare.service';
 import { CreateDialogComponent } from './apointments/dialogs/createDialog/createDialog.component';
